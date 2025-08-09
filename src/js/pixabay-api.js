c525be6da070355ec39748e3346908cdeca4dc7a
Nadiia Rakivska <rakivska94@gmail.com>
2025-08-09 23:49:08 +0300

    hw

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,26 +1,16 @@
 import axios from 'axios';
-import { clearGallery, showLoader } from './render-functions';
+import { clearGallery, hideLoader, showLoader } from './render-functions';
 
 export function getImagesByQuery(query) {
-  showLoader();
-  clearGallery();
   const BASE_URL = 'https://pixabay.com';
   const END_POINT = '/api/';
   const params = new URLSearchParams({
     q: query,
     key: '51658185-19ae36e73b0aa030a4154053e',
-    image_type:"photo",
-    orientation:"horizontal",
-    safesearch:true
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
   });
-
   const url = `${BASE_URL}${END_POINT}?${params}`;
-  return axios
-    .get(url)
-    .then(function (response) {
-      return response;
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
+  return axios.get(url).then(response => response.data.hits);
 }
--- src/main.js
+++ src/main.js
@@ -1,48 +1,56 @@
-import {  getImagesByQuery } from './js/pixabay-api';
+import { getImagesByQuery } from './js/pixabay-api';
 import iziToast from 'izitoast';
 
 import 'izitoast/dist/css/iziToast.min.css';
-import { clearGallery, createGallery, hideLoader } from './js/render-functions';
+import {
+  clearGallery,
+  createGallery,
+  hideLoader,
+  showLoader,
+} from './js/render-functions';
 
 export const refs = {
   galleryElem: document.querySelector('.gallery'),
   loaderJs: document.querySelector('.loader-js'),
-  formEl : document.querySelector('.form')
+  formEl: document.querySelector('.form'),
 };
-;
-refs.formEl.addEventListener("submit", e => {
+refs.formEl.addEventListener('submit', e => {
   e.preventDefault();
-  
-  const query = e.target.elements["search-text"].value.trim();
-  let images = [];
+  const query = e.target.elements['search-text'].value.trim();
+  clearGallery();
+  showLoader();
   if (query.length !== 0) {
-     getImagesByQuery(query).then(data => {
-       hideLoader();
-       for (const item of data.data.hits) {
-         images.push(item);
-       }
-       if (images.length === 0) {
-         iziToast.error({
-           message:
-             'Sorry, there are no images matching your search query. Please try again!',
-           position: 'topRight',
-           icon: 'error',
-           backgroundColor: '#ef4040',
-           iconColor:"white",
-           messageColor: 'white',
-         });
-       } else {
-         createGallery(images);
-       }
-
-       images = [];
-     });
+    getImagesByQuery(query)
+      .then(images => {
+        if (images.length > 0) {
+          createGallery(images);
+        } else {
+          iziToast.error({
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topRight',
+            icon: 'error',
+            backgroundColor: '#ef4040',
+            iconColor: 'white',
+            messageColor: 'white',
+          });
+        }
+      })
+      .catch(error => {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+          icon: 'error',
+          backgroundColor: '#ef4040',
+          iconColor: 'white',
+          messageColor: 'white',
+        });
+        console.error(error);
+      })
+      .finally(() => {
+        hideLoader();
+      });
   }
- 
   e.currentTarget.reset();
-})
-
-
-
-
-
+});
